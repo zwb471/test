900448f38e2bc9f25177721ce8b44f1985b58598
zwb471 <577219099@qq.com>
2022-05-11 16:51:06 +0800

    556

--- untils.js
+++ untils.js
@@ -8,7 +8,10 @@ export function checkArray(key) {
     //判断当前用户权限是否在权限数组中
     let index = arr1.findIndex(item => {
         return item == key
-    })
+    });
+    // let index = arr1.findIndex(item => {
+    //     return item == key
+    // }) //如果在返回索引值，如果不在返回-1
     if (index == -1) {
         return false
     }
