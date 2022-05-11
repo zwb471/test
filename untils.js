f279265ed5e7938a873f631d8fb17325e64ec94d
zwb471 <577219099@qq.com>
2022-05-11 17:16:18 +0800

    hot-fix 4

--- untils.js
+++ untils.js
@@ -13,6 +13,8 @@ export function checkArray(key) {
     //     return item == key
     // }) //如果在返回索引值，如果不在返回-1
     //557777
+    //5587778888888888888
+    console.log('git4')
     if (index == -1) {
         return false
     }
