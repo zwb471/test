68e61d01a0462281579ccb5f3c501f0cdb46a5e8
zwb471 <577219099@qq.com>
2022-05-11 11:42:29 +0800

    258

--- .history/index_20220511114105.js
+++ .history/index_20220511114105.js
@@ -2,7 +2,4 @@ var cc = 12;
 console.log(cc);
 for(var i =0; i<10 ;i++ ){
 console.log(1111111111111111)
-
-console.log(123222222);
-
 } ;
--- .history/index_20220511114151.js
+++ .history/index_20220511114151.js
@@ -3,6 +3,5 @@ console.log(cc);
 for(var i =0; i<10 ;i++ ){
 console.log(1111111111111111)
 
-console.log(123222222);
-
+c
 } ;
