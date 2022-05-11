f80c5fdaece46d1a19c7c7872b7502f30d25b11d
zwb471 <577219099@qq.com>
2022-05-11 16:35:44 +0800

    5555

---
+++ untils.js
@@ -0,0 +1,16 @@
+export function checkArray(key) {
+    //权限数组
+    let arr1 = ['1','3','4'] //后台返回的权限数组
+
+    //当前用户权限
+    // let arr2 = ['1','3','4']
+
+    //判断当前用户权限是否在权限数组中
+    let index = arr1.findIndex(item => {
+        return item == key
+    })
+    if (index == -1) {
+        return false
+    }
+    return true
+}
